45ae874fd1539b41db1ad713122bb6829c67b17d
fab <Pr3dat3ur@hotmail.com>
2021-08-05 14:18:38 +0200

    tf files populated

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,17 +1,17 @@
 variable "security_group_id" {
-  default = "sg-0cfa06fed7a4649c7"
+  default = "subnet-0d3852432b9c60cc2"
 }
 
 variable "subnet_id" {
-  default = "subnet-02db31a87afc4dc31"
+  default = "subnet-0d3852432b9c60cc2"
 }
 
 variable "region" {
-  default = "eu-west-2"
+  default = "eu-west-1"
 }
 
 variable "UNIQUE_ANIMAL_IDENTIFIER" {
-  default = "playground"
+  default = "hercule"
 }
 variable "ARTIFACT" {
   
